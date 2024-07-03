05b444ada42a308be39c76c3a2ef7a365acc56f0
Reinaldo Aparecido Fernandes <reinaldo.fernandes@kobel.com.br>
2024-07-02 22:55:04 -0300

    README.MD
    Documentação do Projeto de AULAS DE API

--- cypress/e2e/Post e Put  Bearer token/put.bearer.token.cy.js
+++ cypress/e2e/Post e Put  Bearer token/put.bearer.token.cy.js
@@ -6,10 +6,10 @@ it('create user with fixture another option', () => {
     for (var i = 0; i < 10; i++)
         randomText += pattern.charAt(Math.floor(Math.random() * pattern.length));
     testEmail = randomText + '@gmail.com'
-    
+
     cy.fixture('createuser').then((payload) => {
         // Cypress fornece um diretório nomeado como fixtures, que armazena vários arquivos "JSON",
-        //e esses arquivos JSON podem armazenar os dados de teste que podem ser lidos por vários testes.
+        // e esses arquivos JSON podem armazenar os dados de teste que podem ser lidos por vários testes.
         // Armazenamos dados de teste na forma de valores-chave, que podemos acessar nos scripts de teste.
 
         cy.api({
@@ -30,8 +30,8 @@ it('create user with fixture another option', () => {
             expect(res.status).to.eq(201)
             // expect (res.body).has.property('email', 'reinaldo.fernandes@kobel.com.br')
             expect(res.body).has.property('email', testEmail)
-            const userId = res.body.id 
-            cy.request({
+            const userId = res.body.id
+            cy.api({
                 method: 'PUT',
                 url: 'https://gorest.co.in/public/v2/users/' + userId,
                 headers: {
@@ -54,4 +54,32 @@ it('create user with fixture another option', () => {
             })
         })
     })
+})
+
+it('API KEY com postman', () => {
+    cy.api({
+        method: 'GET',
+        url: 'https://api.getpostman.com/collections',
+        headers: {
+            'x-api-key': "PMAK-668341f2c87c2f0001118043-b3523d0d591d7e00bb357d5ab5b667b1cb"
+        }
+    }).then((res) => {
+        cy.log(JSON.stringify(res))
+        expect(res.status).to.eq(200)
+    })
+})
+
+it('API KEY com weatherapi Query params', () => {
+    cy.api({
+        method: 'GET',
+        url: 'https://api.openweathermap.org/data/2.5/weather?lat=-20.506980&lon=-47.410460',
+        qs: {
+            'appid': "8481569d3895e0880e4c71790864e521"
+        }
+
+    }).then((res) => {
+        cy.log(JSON.stringify(res))
+        expect(res.status).to.eq(200)
+    })
+
 })
